6eaf928df9ce003b020e412ab6530a0972e060a3
Michael W <mwerbo2@gmail.com>
2018-10-18 19:56:50 -0500

    add results component

---
+++ src/components/Results/Results.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+const TileStyle = {
+    height: '30%',
+    width: '80%',
+    backgroundColor: '#f8f8f8'
+}
+
+const Results = () => {
+    return (
+        <View style={TileStyle}> 
+            <Text>testing</Text>    
+        </View>
+    )
+}
+
+
+
+export default Results;
