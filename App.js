8056d8c84f7863c06d336a9eb03a3487173010f5
theRealGerph <jvongkoth@gmail.com>
2018-10-18 20:24:44 -0500

    added text input box

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Results from './src/components/Results/Results';
+import SearchBar from "./src/components/SearchBar/SearchBar";
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+      <SearchBar />
         <Results />
+
       </View>
     );
   }
