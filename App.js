8af103b8835793e3f6ae7268711231fd32dd88a6
Ken <khuynh024@gmail.com>
2018-10-20 16:35:30 -0500

    create base login screen

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import Results from './src/components/Results/Results';
 import Register from './src/components/Register/Register';
-
+import Login from "./src/components/Login/Login";
 
 
 export default class App extends React.Component {
@@ -10,8 +10,8 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         {/* <Results /> */}
-        <Register/>
-
+        {/* <Register/> */}
+        <Login/>
       </View>
     );
   }
---
+++ src/components/Login/Login.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+
+const Login = () => {
+
+    return (
+        <View>
+            {/* <Button>Continue with Github</Button>
+            <Button>Sign Up with Email</Button> */}
+
+        </View>
+
+    ) 
+
+}
+
+export default Login;
