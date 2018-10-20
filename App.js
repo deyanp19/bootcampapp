9386087eb806fb8e129db3542d3158f456700ea2
Ken <khuynh024@gmail.com>
2018-10-20 16:26:24 -0500

    starting work on register screen

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import Results from './src/components/Results/Results';
+import Register from './src/components/Register/Register';
 
 
 
@@ -8,7 +9,9 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Results />
+        {/* <Results /> */}
+        <Register/>
+
       </View>
     );
   }
