3b9fbca955197eb9ee92bed7e4a7571514b9922e
Ken <khuynh024@gmail.com>
2018-10-22 11:14:30 -0500

    refined results page

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import Results from './src/components/Results/Results';
-import Register from './src/components/Register/Register';
-import Login from "./src/components/Login/Login";
+// import Register from './src/components/Register/Register';
+// import Login from "./src/components/Login/Login";
 import SearchBar from "./src/components/SearchBar/SearchBar";
-
+import Header from "./src/components/Header/Header";
 
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-      <SearchBar />
+      <View style={styles.appStyle}>
+        <Header
+          headerText={"Bootcamps"}
+        />
+        {/* <SearchBar /> */}
         <Results />
 
       </View>
@@ -19,14 +22,11 @@ export default class App extends React.Component {
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
+const styles = {
+  appStyle:{
     flex: 1,
-    flexDirection:'column',
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    // justifyContent: 'center',
-    justifyContent: 'flex-start',
-    marginTop: "10%"
-  },
-});
+    backgroundColor: '#B2DBD5'
+
+
+  }
+}
--- src/components/Results/ResultsTile.js
+++ src/components/Results/ResultsTile.js
@@ -1,17 +1,59 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import data from '../../../data.json';
+import { StyleSheet, Text, View, Image , Linking} from 'react-native';
+// import data from '../../../data.json';
+import Button from '../Button/Button';
+import ResultsSection from './ResultsSection';
 
 const ResultsTile = (props) => {
     return (
-        <View>
-            <Text>{props.title}</Text>
-            <Text>{props.artist}</Text>
-            <Text>{props.url}</Text>  
-            <Image source={{uri: props.image}}
-       style={{width: '30%', height: '30%'}} />
+        <View style={styles.resultsTileStyle}>
+            <ResultsSection>
+                <Text style={styles.textStyle}>{props.title}</Text>
+                <Text style={styles.textStyle}>{props.title}</Text>
+
+            </ResultsSection>
+
+            <ResultsSection>
+                <Text style={styles.textStyle}>{props.artist}</Text>
+            </ResultsSection>
+
+            <ResultsSection>
+                <Text style={styles.textStyle}>{props.url}</Text>
+            </ResultsSection>
+
+            <ResultsSection>
+                <Text style={styles.textStyle}>$15,000</Text>
+
+                <Button 
+                    onPress={() => Linking.openURL(props.url)}>
+                    View Bootcamp Page
+                </Button>            
+            </ResultsSection>  
+                
+                               
        </View>
     )
+}
+
+
+const styles = {
+    resultsTileStyle:{
+        backgroundColor: "#ffffff",
+        marginLeft: 12,
+        marginRight: 12,
+        marginBottom: 10,
+        elevation: 1,
+        justifyContent: 'space-between',
+        padding: 10,
+        borderRadius: 8,
+        flex: 1,
+        flexDirection: 'column'
+    },
+    textStyle:{
+        fontSize: 16,
+        justifyContent: 'space-evenly',
+
+    }
 }
 
 export default ResultsTile;
