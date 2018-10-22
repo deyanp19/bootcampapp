56024b2dd2f18db4940a5c4da5a10170e12f2cfb
theRealGerph <jvongkoth@gmail.com>
2018-10-21 20:16:26 -0500

    added a border to the btn

--- src/components/Filters/FilterBtn.js
+++ src/components/Filters/FilterBtn.js
@@ -2,17 +2,21 @@ import React from 'react';
 import { View, Button } from 'react-native';
 
 const BtnStyle = {
-    border: "1px black"
+//  backgroundColor: "green",
+borderRadius: 4,
+borderWidth: 0.5,
+borderColor: '#d6d7da',
 }
 const FilterBtn = (props) => {
     return(
-        <View>
+        <View
+        style={BtnStyle}>
             <Button
 //   onPress={onPressLearnMore}
   title="Learn More"
   color="#841584"
   accessibilityLabel="Learn more about this purple button"
-  style={BtnStyle}
+  
 />
             </View>
     )
