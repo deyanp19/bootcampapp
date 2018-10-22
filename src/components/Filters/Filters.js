5138bc054f08c80059183fefbb05207f498c4de1
theRealGerph <jvongkoth@gmail.com>
2018-10-21 19:31:24 -0500

    set up constructor and state

---
+++ src/components/Filters/Filters.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+
+export default class Filters extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            filters: filters
+        }
+    }
+}
