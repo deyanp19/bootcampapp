cb9edd0436d033cafc4ab3b62dc30ef859390d35
theRealGerph <jvongkoth@gmail.com>
2018-10-22 19:23:03 -0500

    fixing positioning of searchbar and icon

--- src/components/Search/SearchBar.js
+++ src/components/Search/SearchBar.js
@@ -1,9 +1,5 @@
 import React, { Component } from "react";
 import { AppRegistry, TextInput, View } from 'react-native';
-import Icon from'react-native-vector-icons/FontAwesome';
-import Button from '../Button/Button';
-
-const glass = (<Icon name = "search" size={30} color="#FA8D62" />);
 
 export default class SearchBar extends Component {
     constructor(props) {
@@ -17,7 +13,7 @@ export default class SearchBar extends Component {
         return (
             <View>
             <TextInput
-                style={TextInputStyle}
+                
                 onChangeText={(text) => this.setState({ text })}
                 value={this.state.text}>
             </TextInput>
@@ -28,16 +24,3 @@ export default class SearchBar extends Component {
 }
 
 
-const TextInputStyle = {
-    height: 40,
-    // borderColor: 'black',
-    // borderWidth: 1,
-    width: "70%",
-    marginLeft: 15,
-    // marginRight: 80,
-    backgroundColor: 'white',
-    borderRadius: 5,
-    paddingLeft: 15,
-    marginBottom: 20,
-    marginTop: 10
-}
--- src/components/Search/SearchSection.js
+++ src/components/Search/SearchSection.js
@@ -1,17 +1,37 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import SearchBar from './SearchBar';
-
+import Icon from'react-native-vector-icons/FontAwesome';
+const glass = (<Icon name ="search" size={20} color="#FA8D62"/>)
 
 const SearchSection = (props) => {
 
     return(
-        <View>
+        <View style={{flexDirection: 'row'}}>
+        <View style={TextInputStyle}>
             {props.children}
-        <SearchBar/>
+        <SearchBar />
         </View>
+        <View>
+
+            </View>
+            </View>
     )
 
+}
+const TextInputStyle = {
+    
+    height: 40,
+    // borderColor: 'black',
+    // borderWidth: 1,
+    width: "70%",
+    marginLeft: 15,
+    // marginRight: 80,
+    backgroundColor: 'white',
+    borderRadius: 5,
+    paddingLeft: 15,
+    marginBottom: 20,
+    marginTop: 10
 }
 
 
