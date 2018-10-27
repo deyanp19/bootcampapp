63c079bdcc4e32c1b755f01aeffcc03ad1bba783
theRealGerph <jvongkoth@gmail.com>
2018-10-27 10:41:59 -0500

    created ResultsCommentBox component inside Results dir

---
+++ src/components/Results/ResultsCommentBox.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { TextInput, View } from 'react-native';
+
+export default class ResultCommentBox extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            text: ''
+        }
+    }
+
+    render() {
+
+        return (
+            <View>
+                <TextInput
+                    onChange={(text) => this.setState({ text })}
+                    value={this.state.text}
+                    placeholder='Leave a comment' />
+
+            </View>
+        );
+    }
+}
--- src/components/Results/ResultsTile.js
+++ src/components/Results/ResultsTile.js
@@ -1,62 +1,113 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image , Linking} from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image, Linking, LayoutAnimation, Platform, UIManager, TouchableOpacity } from 'react-native';
 // import data from '../../../data.json';
 import Button from '../Button/Button';
 import ResultsSection from './ResultsSection';
-import Icon from'react-native-vector-icons/FontAwesome';
-const star = (<Icon name ="star" size={20} color="#FA8D62"/>)
-const comment = (<Icon name ="comment" size={20} color="#FA8D62"/>)
+import Icon from 'react-native-vector-icons/FontAwesome';
+const star = (<Icon name="star" size={20} color="#FA8D62" />)
+const comment = (<Icon name="comment" size={20} color="#FA8D62" />)
+const browser = (<Icon name="file" size={20} />)
 
 
-const ResultsTile = (props) => {
-    return (
-        <View style={styles.resultsTileStyle}>
-            <ResultsSection>
-                <Text style={styles.textStyle}>{props.name}</Text>
-                <Text style={styles.textStyle}>{props.location}</Text>
+export default class ResultsTile extends Component {
+    constructor()
+    {
+        super();
+ 
+        this.state = { 
+            // onLayoutHeight: 0,
+            modifiedHeight: 0,
+            expanded: false 
+        }
+ 
+        if( Platform.OS === 'android' )
+        {
+          UIManager.setLayoutAnimationEnabledExperimental( true )
+        }
+    }
+ 
+    changeLayout = () =>
+    {
+        LayoutAnimation.configureNext( LayoutAnimation.Presets.easeInEaseOut );
+ 
+        if( this.state.expanded === false )
+            this.setState({ modifiedHeight: 100, expanded: true });
+        else
+            this.setState({ modifiedHeight: 0, expanded: false });
+    }
+ 
+    getViewHeight( height )
+    {
+        this.setState({ onLayoutHeight: height });
+    }
+    
+    render() {
 
-            </ResultsSection>
 
-            <ResultsSection>
-                <Text style={styles.textStyle}>{props.topics}</Text>
-            </ResultsSection>
+        return (
+            <View style={styles.resultsTileStyle}>
+            <TouchableOpacity activeOpacity = { 0.8 } onPress = { this.changeLayout }>
+                <ResultsSection>
+                    <Text style={styles.textStyle}>{this.props.name}</Text>
+                    <Text style={styles.textStyle}>{this.props.location}</Text>
 
-            {/* <ResultsSection>
-                <Text style={styles.textStyle}>{props.url}</Text>
+                </ResultsSection>
+
+                <ResultsSection>
+                    <Text style={styles.textStyle}>{this.props.topics}</Text>
+                </ResultsSection>
+
+                {/* <ResultsSection>
+                <Text style={styles.textStyle}>{this.props.url}</Text>
             </ResultsSection> */}
 
-            <ResultsSection>
-                <Text style={styles.textStyle}>$15,000</Text>
+                <ResultsSection>
+                    <Text style={styles.textStyle}>$15,000</Text>
 
-                {/* <Button 
-                    onPress={() => Linking.openURL(props.url)}>
+                    {/* <Button 
+                    onPress={() => Linking.openURL(this.props.url)}>
                     View Bootcamp Page
                 </Button>         */}
-                <ResultsSection>
-                    <Button 
-                        onPress={() => Linking.openURL(props.url)}>
+                    <ResultsSection>
+                        {/* <Button 
+                        onPress={() => Linking.openURL(this.props.url)}>
                         View Bootcamp Page
-                    </Button>  
+                    </Button>   */}
 
-                    <Button>
+                        {/* <Button>
                         {comment} 
-                    </Button> 
+                    </Button>  */}
 
-                    <Button>
-                        {star}
-                    </Button> 
+                        <Button>
+                            {browser}
+                        </Button>
+                        <Button>
+                            {star}
+                        </Button>
+                    </ResultsSection>
+
                 </ResultsSection>
-     
-            </ResultsSection>  
-                
-                               
-       </View>
-    )
+
+            </TouchableOpacity>
+
+                                <View style = {{ height: this.state.modifiedHeight, overflow: 'hidden' }}>
+                        <Text style = { styles.text } onLayout = {( event ) => this.getViewHeight( event.nativeEvent.layout.height )}>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                            when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            It has survived not only five centuries, but also the leap into electronic typesetting, 
+                            remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets 
+                            containing Lorem Ipsum passages, and more recently with desktop publishing software 
+                            like Aldus PageMaker including versions of Lorem Ipsum.
+                        </Text>
+                        </View>
+            </View>
+        )
+    }
 }
 
-
 const styles = {
-    resultsTileStyle:{
+    resultsTileStyle: {
         backgroundColor: "#ffffff",
         marginLeft: 12,
         marginRight: 12,
@@ -68,11 +119,10 @@ const styles = {
         flex: 1,
         flexDirection: 'column'
     },
-    textStyle:{
+    textStyle: {
         fontSize: 16,
         justifyContent: 'space-evenly',
 
     }
 }
 
-export default ResultsTile;
