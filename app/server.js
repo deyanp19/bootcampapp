956ba7607e3964a396167d2d61c5b2620c823fb0
Michael W <mwerbo2@gmail.com>
2018-10-30 01:41:17 -0500

    Set up server to direct to routes

--- app/server.js
+++ app/server.js
@@ -5,32 +5,15 @@ const bodyParser = require('body-parser');
 const app = express()
 const port = process.env.PORT || 3000;
 
+// const bootcamps = require('./server/routes/index.js');
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => res.status(200).send({ message: 'Hello World!'}));
+require('./server/routes')(app)
+app.get('*', (req, res) => res.status(200).send({ message: 'Bootcamp app!'}));
 
 app.listen(port, () => console.log(`Listening on ${port}`))
 
 module.exports = app;
-
-// const Sequelize = require('sequelize');
-// const sequelize = new Sequelize('database', 'username', 'password', {
-//   host: 'localhost',
-//   dialect: 'mysql'|'sqlite'|'postgres'|'mssql',
-//   operatorsAliases: false,
-
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000
-//   },
-
-//   // SQLite only
-//   storage: 'path/to/database.sqlite'
-// });
-
-// Or you can simply use a connection uri
-// const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname');
